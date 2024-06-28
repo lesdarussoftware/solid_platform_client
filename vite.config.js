f0c3f68c92b2c6a1caf1037c164fdf7d70648ab1
matias ramos <matiasramos261@gmail.com>
2024-06-28 18:10:44 -0300

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/solid-platform/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        "name": "Solid Platform",
+        "short_name": "Solid",
+        "start_url": "/solid-platform",
+        "display": "standalone",
+        "background_color": "#ffffff",
+        "theme_color": "#000000",
+        "icons": [
+          {
+            "src": "/logo.png",
+            "sizes": "197x86",
+            "type": "image/png"
+          }
+        ]
+      },
+      registerType: 'autoUpdate',
+      workbox: {
+
+      }
+    })
+  ],
 })
