4b6432a5c1a77531b9556d3652c9f90cd933496c
matias ramos <matiasramos261@gmail.com>
2024-07-08 12:23:25 -0300

    fix socket

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { AuthProvider } from "./providers/AuthProvider"
 import { DataProvider } from "./providers/DataProvider"
 import { MessageProvider } from "./providers/MessageProvider"
-import { NotificationsProvider } from "./providers/NotificationsProvider"
+// import { NotificationsProvider } from "./providers/NotificationsProvider"
 
 import { Home } from "./pages/Home"
 import { Dashboard } from "./pages/Dashboard"
@@ -28,17 +28,17 @@ function App() {
       <AuthProvider>
         <DataProvider>
           <MessageProvider>
-            <NotificationsProvider>
-              <BrowserRouter>
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/dashboard" element={<Dashboard />} />
-                  <Route path="/scanner" element={<Scan />} />
-                  <Route path="/perfil" element={<Profile />} />
-                  <Route path="*" element={<Error />} />
-                </Routes>
-              </BrowserRouter>
-            </NotificationsProvider>
+            {/* <NotificationsProvider> */}
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/scanner" element={<Scan />} />
+                <Route path="/perfil" element={<Profile />} />
+                <Route path="*" element={<Error />} />
+              </Routes>
+            </BrowserRouter>
+            {/* </NotificationsProvider> */}
           </MessageProvider>
         </DataProvider>
       </AuthProvider>
