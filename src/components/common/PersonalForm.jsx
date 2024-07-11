3469b2227677c5cc03c443ce8e9670825d2ea1a7
matias ramos <matiasramos261@gmail.com>
2024-07-11 13:25:27 -0300

    fix

--- src/components/common/PersonalForm.jsx
+++ src/components/common/PersonalForm.jsx
@@ -65,32 +65,20 @@ export function PersonalForm({
                 </Box>
                 <Box sx={{ display: 'flex', justifyContent: 'space-between', gap: 1 }}>
                     <FormControl sx={{ width: '33%' }}>
+                        <InputLabel htmlFor="cell_phone">Celular</InputLabel>
+                        <Input id="cell_phone" type="text" name="cell_phone" value={formData.cell_phone} />
+                        {errors.cell_phone?.type === 'maxLength' &&
+                            <Typography variant="caption" color="red" marginTop={1}>
+                                * El celular es demasiado largo.
+                            </Typography>
+                        }
+                    </FormControl>
+                    <FormControl sx={{ width: '33%' }}>
                         <InputLabel htmlFor="cuil">CUIL</InputLabel>
                         <Input id="cuil" type="text" name="cuil" value={formData.cuil} />
                         {errors.cuil?.type === 'maxLength' &&
                             <Typography variant="caption" color="red" marginTop={1}>
                                 * El cuil es demasiado largo.
-                            </Typography>
-                        }
-                    </FormControl>
-                    <FormControl sx={{ width: '33%' }}>
-                        <InputLabel id="category-select">Categoría</InputLabel>
-                        <Select
-                            labelId="category-select"
-                            id="category"
-                            value={formData.category_id}
-                            label="Categoría"
-                            name="category"
-                            onChange={e => handleChange({ target: { name: 'category_id', value: e.target.value } })}
-                        >
-                            <MenuItem value="">Seleccione</MenuItem>
-                            {state.categories.map(cat => (
-                                <MenuItem key={cat.id} value={cat.id}>{cat.name}</MenuItem>
-                            ))}
-                        </Select>
-                        {errors.site_id?.type === 'required' &&
-                            <Typography variant="caption" color="red" marginTop={1}>
-                                * El evento es requerido.
                             </Typography>
                         }
                     </FormControl>
@@ -105,6 +93,44 @@ export function PersonalForm({
                         </LocalizationProvider>
                     </FormControl>
                 </Box>
+                {fromWorkers &&
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between', gap: 1 }}>
+                        <FormControl sx={{ width: '33%' }}>
+                            <InputLabel id="category-select">Categoría</InputLabel>
+                            <Select
+                                labelId="category-select"
+                                id="category"
+                                value={formData.category_id}
+                                label="Categoría"
+                                name="category"
+                                onChange={e => handleChange({ target: { name: 'category_id', value: e.target.value } })}
+                            >
+                                <MenuItem value="">Seleccione</MenuItem>
+                                {state.categories.map(cat => (
+                                    <MenuItem key={cat.id} value={cat.id}>{cat.name}</MenuItem>
+                                ))}
+                            </Select>
+                            {errors.site_id?.type === 'required' &&
+                                <Typography variant="caption" color="red" marginTop={1}>
+                                    * El evento es requerido.
+                                </Typography>
+                            }
+                        </FormControl>
+                        <FormControl sx={{ width: '33%' }}>
+                            <InputLabel htmlFor="regime">Régimen</InputLabel>
+                            <Input id="regime" type="number" name="regime" value={formData.regime} />
+                        </FormControl>
+                        <FormControl sx={{ width: '33%' }}>
+                            <InputLabel htmlFor="observations">Observaciones</InputLabel>
+                            <Input id="observations" type="text" name="observations" value={formData.observations} />
+                            {errors.observations?.type === 'maxLength' &&
+                                <Typography variant="caption" color="red" marginTop={1}>
+                                    * Las observaciones son demasiado largas.
+                                </Typography>
+                            }
+                        </FormControl>
+                    </Box>
+                }
                 <Box sx={{ display: 'flex', justifyContent: 'space-between', gap: 1 }}>
                     <FormControl sx={{ width: '33%' }}>
                         <InputLabel htmlFor="address">Dirección</InputLabel>
@@ -124,33 +150,7 @@ export function PersonalForm({
                             </Typography>
                         }
                     </FormControl>
-                    <FormControl sx={{ width: '33%' }}>
-                        <InputLabel htmlFor="cell_phone">Celular</InputLabel>
-                        <Input id="cell_phone" type="text" name="cell_phone" value={formData.cell_phone} />
-                        {errors.cell_phone?.type === 'maxLength' &&
-                            <Typography variant="caption" color="red" marginTop={1}>
-                                * El celular es demasiado largo.
-                            </Typography>
-                        }
-                    </FormControl>
                 </Box>
-                {fromWorkers &&
-                    <Box sx={{ display: 'flex', justifyContent: 'space-between', gap: 1 }}>
-                        <FormControl sx={{ width: '33%' }}>
-                            <InputLabel htmlFor="regime">Régimen</InputLabel>
-                            <Input id="regime" type="number" name="regime" value={formData.regime} />
-                        </FormControl>
-                        <FormControl sx={{ width: '66%' }}>
-                            <InputLabel htmlFor="observations">Observaciones</InputLabel>
-                            <Input id="observations" type="text" name="observations" value={formData.observations} />
-                            {errors.observations?.type === 'maxLength' &&
-                                <Typography variant="caption" color="red" marginTop={1}>
-                                    * Las observaciones son demasiado largas.
-                                </Typography>
-                            }
-                        </FormControl>
-                    </Box>
-                }
             </Box>
             <Box sx={{ display: 'flex', gap: 1, marginTop: 2, justifyContent: 'center' }}>
                 <Button
