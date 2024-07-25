abd7592fac057e091018a5a804e7235cf6633b0b
matias ramos <matiasramos261@gmail.com>
2024-07-25 17:46:08 -0300

    pwa

--- src/main.jsx
+++ src/main.jsx
@@ -8,15 +8,15 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register('/solid-platform/sw.js').then(registration => {
-//       console.log('SW registered: ', registration);
-//     }).catch(registrationError => {
-//       console.log('SW registration failed: ', registrationError);
-//     });
-//   });
-// }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // VitePWA({
-    //   manifest: {
-    //     "name": "Solid Platform",
-    //     "short_name": "Solid",
-    //     "start_url": "/solid-platform",
-    //     "display": "standalone",
-    //     "background_color": "#ffffff",
-    //     "theme_color": "#000000",
-    //     "icons": [
-    //       {
-    //         "src": "/logo.jpeg",
-    //         "sizes": "500x500",
-    //         "type": "image/jpeg"
-    //       }
-    //     ]
-    //   },
-    //   registerType: 'autoUpdate'
-    // })
+    VitePWA({
+      manifest: {
+        "name": "Solid Platform",
+        "short_name": "Solid",
+        "start_url": "/",
+        "display": "standalone",
+        "background_color": "#ffffff",
+        "theme_color": "#000000",
+        "icons": [
+          {
+            "src": "/logo.jpeg",
+            "sizes": "500x500",
+            "type": "image/jpeg"
+          }
+        ]
+      },
+      registerType: 'autoUpdate'
+    })
   ],
 })
