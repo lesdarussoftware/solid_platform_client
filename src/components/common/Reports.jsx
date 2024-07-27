ee84b14b95b32429249725ead0d80c4467d55723
matias ramos <matiasramos261@gmail.com>
2024-07-27 15:23:38 -0300

    estilos impresion

--- src/components/common/Reports.jsx
+++ src/components/common/Reports.jsx
@@ -4,19 +4,16 @@ import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers"
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns"
 import { es } from "date-fns/locale"
 
+import { AuthContext } from "../../providers/AuthProvider";
 import { DataContext } from "../../providers/DataProvider";
 import { useForm } from "../../hooks/useForm";
-import { useQuery } from "../../hooks/useQuery";
+
 import { REPORT_URL } from "../../helpers/urls";
-import { MessageContext } from "../../providers/MessageProvider";
-import { AuthContext } from "../../providers/AuthProvider";
 
 export function Reports() {
 
     const { auth } = useContext(AuthContext)
     const { state } = useContext(DataContext)
-
-    const { handleQuery } = useQuery()
 
     const {
         formData: siteStatusData,
@@ -35,46 +32,61 @@ export function Reports() {
                 window.open(`${REPORT_URL}/estado-obra-pdf/${auth?.refresh_token}/${siteStatusData.site}/${siteStatusData.date.toISOString()}`, '_blank')
             }
             if (type === 'EXCEL') {
-                handleQuery({ url: `${REPORT_URL}/estado-obra-excel/${siteStatusData.site}/${siteStatusData.date.toISOString()}` })
+                window.open(`${REPORT_URL}/estado-obra-excel/${auth?.refresh_token}/${siteStatusData.site}/${siteStatusData.date.toISOString()}`, '_blank')
             }
         }
     }
 
     return (
         <Box>
-            <FormControl sx={{ width: '50%' }}>
-                <Autocomplete
-                    disablePortal
-                    id="site-autocomplete"
-                    options={state.sites.map(s => ({ label: s.name, name: s.name }))}
-                    noOptionsText="No hay obras disponibles."
-                    onChange={(_, value) => siteStatusChange({ target: { name: 'site', value: value.name } })}
-                    renderInput={(params) => <TextField {...params} label="Obra" />}
-                    value={siteStatusData.site.toString().length > 0 ? state.sites.find(s => s.name === siteStatusData.site).name : ''}
-                    isOptionEqualToValue={(option, value) => value.length === 0 || option.name === value}
-                />
-                {siteStatusErrors.site?.type === 'required' &&
-                    <Typography variant="caption" color="red" marginTop={1}>
-                        * La obra es requerida.
-                    </Typography>
-                }
-            </FormControl>
-            <FormControl sx={{ width: '50%' }}>
-                <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={es}>
-                    <DatePicker
-                        label="Fecha"
-                        value={new Date(siteStatusData.date)}
-                        name="date"
-                        onChange={value => siteStatusChange({ target: { name: 'date', value: new Date(value.toISOString()) } })}
+            <Box sx={{ width: { xs: '100%', md: '30%' } }}>
+                <Typography variant="h5" marginBottom={2}>Estado de obra</Typography>
+                <FormControl sx={{ width: '100%', marginBottom: 2 }}>
+                    <Autocomplete
+                        disablePortal
+                        id="site-autocomplete"
+                        options={state.sites.map(s => ({ label: s.name, name: s.name }))}
+                        noOptionsText="No hay obras disponibles."
+                        onChange={(_, value) => siteStatusChange({ target: { name: 'site', value: value.name } })}
+                        renderInput={(params) => <TextField {...params} label="Obra" />}
+                        value={siteStatusData.site.toString().length > 0 ? state.sites.find(s => s.name === siteStatusData.site).name : ''}
+                        isOptionEqualToValue={(option, value) => value.length === 0 || option.name === value}
                     />
-                </LocalizationProvider>
-            </FormControl>
-            <Button type="button" variant="contained" onClick={e => siteStatusSubmit(e, 'PDF')}>
-                PDF
-            </Button>
-            <Button type="button" variant="contained" onClick={e => siteStatusSubmit(e, 'EXCEL')}>
-                Excel
-            </Button>
-        </Box>
+                    {siteStatusErrors.site?.type === 'required' &&
+                        <Typography variant="caption" color="red" marginTop={1}>
+                            * La obra es requerida.
+                        </Typography>
+                    }
+                </FormControl>
+                <FormControl sx={{ width: '100%', marginBottom: 2 }}>
+                    <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={es}>
+                        <DatePicker
+                            label="Fecha"
+                            value={new Date(siteStatusData.date)}
+                            name="date"
+                            onChange={value => siteStatusChange({ target: { name: 'date', value: new Date(value.toISOString()) } })}
+                        />
+                    </LocalizationProvider>
+                </FormControl>
+                <Box sx={{ width: '100%', display: 'flex', gap: 1 }}>
+                    <Button
+                        type="button"
+                        variant="contained"
+                        sx={{ width: { xs: '100%', sm: '20%' } }}
+                        onClick={e => siteStatusSubmit(e, 'PDF')}
+                    >
+                        PDF
+                    </Button>
+                    <Button
+                        type="button"
+                        variant="contained"
+                        sx={{ width: { xs: '100%', sm: '20%' } }}
+                        onClick={e => siteStatusSubmit(e, 'EXCEL')}
+                    >
+                        Excel
+                    </Button>
+                </Box>
+            </Box>
+        </Box >
     )
 }
